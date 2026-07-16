e5245da6353c7ca327d5a12d54bbbd39a8f0c839
I-Frostbyte <gilbert.tanuie.tima@gmail.com>
2026-07-16 05:14:18 +0100

    Setting up Sorting Exercise

---
+++ Exercises/2-Sorting_Exercise_Sorting_Program/sort/service.go
@@ -0,0 +1,37 @@
+package sort
+
+type ArraySort struct {
+	input []int
+}
+
+func NewArraySort(
+	input []int,
+	) *ArraySort {
+		return &ArraySort{
+			input: input,
+		}
+}
+
+func (as *ArraySort) BubbleSort(input []int) []int {
+	panic ("unimplemented")
+}
+
+func (as *ArraySort) SelectionSort(input []int) []int {
+	panic ("unimplemented")
+}
+
+func (as *ArraySort) InsertionSort(input []int) []int {
+	panic ("unimplemented")
+}
+
+func (as *ArraySort) ShellSort(input []int) []int {
+	panic ("unimplemented")
+}
+
+func (as *ArraySort) MergeSort(input []int) []int {
+	panic ("unimplemented")
+}
+
+func (as *ArraySort) QuickSort(input []int) []int {
+	panic ("unimplemented")
+}
---
+++ Exercises/2-Sorting_Exercise_Sorting_Program/sort/service_test.go
@@ -0,0 +1,34 @@
+package sort
+
+import (
+	"testing"
+)
+
+type Test struct {
+	name string
+	input []int
+}
+
+func TestBubbleSort(t *testing.T) {
+
+}
+
+func TestSelectionSort(t *testing.T) {
+
+}
+
+func TestInsertionSort(t *testing.T) {
+
+}
+
+func TestShellSort(t *testing.T) {
+
+}
+
+func TestMergeSort(t *testing.T) {
+
+}
+
+func TestQuickSort(t *testing.T) {
+	
+}
