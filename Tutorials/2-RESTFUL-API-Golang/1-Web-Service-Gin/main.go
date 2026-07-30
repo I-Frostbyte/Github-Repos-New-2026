a2f9bbb4769de31551e94d7060186db0a82888f9
I-Frostbyte <gilbert.tanuie.tima@gmail.com>
2026-07-30 08:32:54 +0100

    Gin POST request

--- Tutorials/2-RESTFUL-API-Golang/1-Web-Service-Gin/main.go
+++ Tutorials/2-RESTFUL-API-Golang/1-Web-Service-Gin/main.go
@@ -38,13 +38,32 @@ var albums = []album{
 
 func main() {
 	router := gin.Default()
+	// Associating the GET HTTP method and /albums path with a handler function.
 	router.GET("/albums", getAlbums)
 
+	// Associating the POST HTTP method and /albums path with a handler function.
+	router.POST("/albums", postAlbums)
+
 	fmt.Println("Running server on port: 8282")
+	// Attaching the router to a run server
 	router.Run("localhost:8282")
 }
 
 // getAlbums responds with the list of all albums as JSON.
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// postAlbums adds an album from JSON received in the request bod.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
