3179e2c9b74e556bbfd174e19a338778a987e618
I-Frostbyte <gilbert.tanuie.tima@gmail.com>
2026-07-30 08:51:14 +0100

    GIN Get By ID

--- Tutorials/2-RESTFUL-API-Golang/1-Web-Service-Gin/main.go
+++ Tutorials/2-RESTFUL-API-Golang/1-Web-Service-Gin/main.go
@@ -39,9 +39,15 @@ var albums = []album{
 func main() {
 	router := gin.Default()
 	// Associating the GET HTTP method and /albums path with a handler function.
+	// GET ALL Albums.
 	router.GET("/albums", getAlbums)
 
+	// Associating the GET HTTP method and /albums/:id path with a handler function.
+	// GET specific album with id.
+	router.GET("/albums/:id", getAlbumByID)
+	
 	// Associating the POST HTTP method and /albums path with a handler function.
+	// Create New Album
 	router.POST("/albums", postAlbums)
 
 	fmt.Println("Running server on port: 8282")
@@ -50,20 +56,36 @@ func main() {
 }
 
 // getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+func getAlbums(ctx *gin.Context) {
+	ctx.IndentedJSON(http.StatusOK, albums)
 }
 
 // postAlbums adds an album from JSON received in the request bod.
-func postAlbums(c *gin.Context) {
+func postAlbums(ctx *gin.Context) {
 	var newAlbum album
 
 	// Call BindJSON to bind the received JSON to newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
+	if err := ctx.BindJSON(&newAlbum); err != nil {
 		return
 	}
 
 	// Add the new album to the slice.
 	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
+	ctx.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a reponse.
+func getAlbumByID(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, album := range albums {
+		if album.ID == id {
+			ctx.IndentedJSON(http.StatusOK, album)
+			return
+		}
+	}
+	ctx.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
